82161aa7fd93bb11c77587b96ae7edcf7534701b
David Vielhuber <david@vielhuber.de>
2022-02-25 02:33:39 +0100

    Improve lib.

--- _js/script.js
+++ _js/script.js
@@ -1,138 +1,144 @@
+import hlp from 'hlp';
 export default class Lanimate {
     init() {
-        const ready = new Promise(resolve => {
-            if (document.readyState !== 'loading') {
-                return resolve();
-            } else {
-                document.addEventListener('DOMContentLoaded', () => {
-                    return resolve();
-                });
-            }
-        });
-        ready.then(() => {
+        hlp.ready().then(() => {
             this.ready();
         });
-        const load = new Promise(resolve => {
-            if (document.readyState === 'complete') {
-                return resolve();
-            } else {
-                window.addEventListener('load', () => {
-                    return resolve();
-                });
-            }
-        });
-        load.then(() => {
+        hlp.load().then(() => {
             this.load();
         });
     }
     ready() {
-        document.head.insertAdjacentHTML(
-            'beforeend',
-            `
-            <style>
-                [data-lanimate] {
-                    opacity: 0;
-                }
-                /* disable on mobile */
-                @media screen and (max-width: 1100px) {
-                    [data-lanimate] {
-                        transition: all 0s ease 0s;
-                        opacity: 1;
-                    }
-                    [data-lanimate='scrollX'] {
-                        transform: translateX(0px);
-                    }
-                    [data-lanimate='scrollY'] {
-                        transform: translateX(0px);
-                    }
-                    [data-lanimate='scale'] {
-                        transform: scale(1) translateY(0px);
-                    }
-                    [data-lanimate='rotate'] {
-                        transform: rotate(0deg);
-                    }
-                }
-            </style>
-            `
-        );
-    }
-    traverseAllTextNodes($el, $fn) {
-        if ($el.childNodes.length > 0) {
-            $el.childNodes.forEach($el2 => {
-                if ($el2.nodeType === 3) {
-                    $el2.textContent = $fn($el2.textContent);
-                } else {
-                    this.traverseAllTextNodes($el2, $fn);
-                }
-            });
-        }
+        this.addBasicStyles();
     }
     load() {
-        if (document.querySelectorAll('[data-lanimate-split]').length > 0) {
-            document.querySelectorAll('[data-lanimate-split]').forEach($el => {
+        this.addExtendedStyles();
+        hlp.runForEl('[data-lanimate]', $el => {
+            if ($el.parentNode.closest('[data-lanimate]') !== null) {
+                return;
+            }
+            if ($el.hasAttribute('data-lanimate-split')) {
                 $el.setAttribute('data-aria-label', $el.innerText.replace(/(?:\r\n|\r|\n)/g, ' '));
-                $el.innerHTML = $el.innerHTML.split(' ').join('&nbsp;');
+                $el.innerHTML = $el.innerHTML.trim().replace(/ {2,}/g, '').split(' ').join('&nbsp;');
                 let shift = 0;
                 if ($el.getAttribute('data-lanimate-split') === 'char') {
                     this.traverseAllTextNodes($el, $text => {
                         return '%span%' + $text.split('').join('%/span%%span%') + '%/span%';
                     });
                     $el.innerHTML = $el.innerHTML.split('%span%').join('<span>').split('%/span%').join('</span>');
-                    shift = 100;
+                    shift = 50;
                 }
                 if ($el.getAttribute('data-lanimate-split') === 'word') {
                     this.traverseAllTextNodes($el, $text => {
-                        return '%span%' + $text.split(' ').join('%/span% %span%') + '%/span%';
+                        return '%span%' + $text.split(' ').join('%/span% %span%') + '%/span%';
                     });
                     $el.innerHTML = $el.innerHTML.split('%span%').join('<span>').split('%/span%').join('</span>');
-                    shift = 250;
+                    shift = 75;
                 }
                 if ($el.getAttribute('data-lanimate-split') === 'line') {
                     $el.innerHTML = '<span>' + $el.innerHTML.split('<br>').join('</span><br/><span>') + '</span>';
-                    shift = 500;
+                    shift = 100;
+                }
+                if ($el.hasAttribute('data-lanimate-speed')) {
+                    shift = (shift * parseInt($el.getAttribute('data-lanimate-speed'))) / 100;
                 }
                 let index = 0;
-                $el.querySelectorAll('span').forEach(($el2, i) => {
+                $el.querySelectorAll('span').forEach($el2 => {
                     if ($el2.innerText.trim() == '') {
                         return;
                     }
-                    $el2.setAttribute('data-lanimate', $el.getAttribute('data-lanimate'));
                     let delay = 0;
                     if ($el.hasAttribute('data-lanimate-delay')) {
                         delay = parseInt($el.getAttribute('data-lanimate-delay'));
                     }
                     if ($el.hasAttribute('data-lanimate-speed')) {
                         $el2.setAttribute('data-lanimate-speed', $el.getAttribute('data-lanimate-speed'));
-                        delay *= 1 / parseInt($el.getAttribute('data-lanimate-speed'));
                     }
                     $el2.setAttribute('data-lanimate-delay', delay + shift * index);
+                    $el2.setAttribute('data-lanimate', $el.getAttribute('data-lanimate'));
                     index++;
+                    this.prepareAndBindStart($el2);
                 });
                 $el.removeAttribute('data-lanimate');
                 $el.removeAttribute('data-lanimate-delay');
                 $el.removeAttribute('data-lanimate-speed');
-            });
+            } else {
+                this.prepareAndBindStart($el);
+            }
+        });
+    }
+    prepareAndBindStart($el) {
+        if ($el.hasAttribute('data-lanimate-speed')) {
+            let speed = parseInt($el.getAttribute('data-lanimate-speed')) / 1000;
+            setTimeout(() => {
+                $el.style.transitionDuration = speed + 's, ' + speed + 's';
+            }, 10);
         }
-        if (document.querySelectorAll('[data-lanimate-speed]').length > 0) {
-            document.querySelectorAll('[data-lanimate-speed]').forEach($el => {
-                let speed = parseInt($el.getAttribute('data-lanimate-speed')) / 1000;
-                setTimeout(() => {
-                    $el.style.transitionDuration = speed + 's, ' + speed + 's';
-                }, 10);
-            });
+        this.start($el);
+        window.addEventListener('scroll', () => {
+            this.start($el);
+        });
+    }
+    start($el) {
+        if (
+            !$el.hasAttribute('data-lanimate-started') &&
+            this.scrollTop() + window.innerHeight >= this.offsetTop($el)
+        ) {
+            let delay = 0;
+            if ($el.hasAttribute('data-lanimate-delay')) {
+                delay = parseInt($el.getAttribute('data-lanimate-delay'));
+            }
+            setTimeout(() => {
+                $el.setAttribute('data-lanimate-started', 'true');
+            }, delay);
         }
-        if (document.querySelectorAll('[data-lanimate]').length > 0) {
-            document.querySelectorAll('[data-lanimate]').forEach($el => {
-                this.start($el);
-                window.addEventListener('scroll', () => {
-                    this.start($el);
-                });
-            });
-        }
-        document.head.insertAdjacentHTML(
-            'beforeend',
+    }
+    scrollTop() {
+        let doc = document.documentElement;
+        return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    }
+    offsetTop($el) {
+        return $el.getBoundingClientRect().top + window.pageYOffset - document.documentElement.clientTop;
+    }
+    addBasicStyles() {
+        if (document.head.querySelector('.lanimate-styles-basic') === null) {
+            document.head.insertAdjacentHTML(
+                'beforeend',
+                `
+            <style class="lanimate-styles-basic">
+                [data-lanimate] {
+                    opacity: 0;
+                }
+                /* disable on mobile */
+                @media screen and (max-width: 1100px) {
+                    [data-lanimate] {
+                        transition: all 0s ease 0s !important;
+                        opacity: 1 !important;
+                    }
+                    [data-lanimate='scrollX'] {
+                        transform: translateX(0px) !important;
+                    }
+                    [data-lanimate='scrollY'] {
+                        transform: translateX(0px) !important;
+                    }
+                    [data-lanimate='scale'] {
+                        transform: scale(1) translateY(0px) !important;
+                    }
+                    [data-lanimate='rotate'] {
+                        transform: rotate(0deg) !important;
+                    }
+                }
+            </style>
             `
-            <style>
+            );
+        }
+    }
+    addExtendedStyles() {
+        if (document.head.querySelector('.lanimate-styles-extended') === null) {
+            document.head.insertAdjacentHTML(
+                'beforeend',
+                `
+            <style class="lanimate-styles-extended">
                 [data-lanimate='fade'] {
                 }
                 [data-lanimate='scrollX'] {
@@ -145,7 +151,7 @@ export default class Lanimate {
                     transform: scale(1.1) translateY(30px);
                 }
                 [data-lanimate='rotate'] {
-                    transform: rotate(15deg);
+                    transform: rotate(10deg);
                     transform-origin: 0 0;
                 }
                 [data-lanimate-started] {
@@ -171,28 +177,19 @@ export default class Lanimate {
                 }
             </style>
             `
-        );
-    }
-    start($el) {
-        if (
-            !$el.hasAttribute('data-lanimate-started') &&
-            this.scrollTop() + window.innerHeight >= this.offsetTop($el)
-        ) {
-            let delay = 0;
-            if ($el.hasAttribute('data-lanimate-delay')) {
-                delay = parseInt($el.getAttribute('data-lanimate-delay'));
-            }
-            setTimeout(() => {
-                $el.setAttribute('data-lanimate-started', 'true');
-            }, delay);
+            );
         }
     }
-    scrollTop() {
-        let doc = document.documentElement;
-        return (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-    }
-    offsetTop($el) {
-        return $el.getBoundingClientRect().top + window.pageYOffset - document.documentElement.clientTop;
+    traverseAllTextNodes($el, $fn) {
+        if ($el.childNodes.length > 0) {
+            $el.childNodes.forEach($el2 => {
+                if ($el2.nodeType === 3) {
+                    $el2.textContent = $fn($el2.textContent);
+                } else {
+                    this.traverseAllTextNodes($el2, $fn);
+                }
+            });
+        }
     }
 }
 
